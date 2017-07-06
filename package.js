da19599b45a7b48cf6b480e8bce15c8531f849fb
BaobabCoder <b.campardou@gmail.com>
2017-07-06 15:47:11 +0200

    Allow to use other fonts that fontawesome

--- package.js
+++ package.js
@@ -1,38 +1,37 @@
 Package.describe({
-  name: 'themeteorchef:bert',
-  version: '2.1.3',
-  summary: 'A client side, multi-style alerts system for Meteor.',
-  git: 'http://github.com/themeteorchef/bert',
-  documentation: 'README.md'
+    name: 'baobabcoder:bert',
+    version: '2.1.3',
+    summary: 'A client side, multi-style alerts system for Meteor.',
+    git: 'http://github.com/themeteorchef/bert',
+    documentation: 'README.md'
 });
 
-Package.onUse( function( api ) {
-  api.versionsFrom( '1.2.0.2' );
+Package.onUse(function(api) {
+    api.versionsFrom('1.2.0.2');
 
-  api.use([
-    'ecmascript',
-    'templating',
-    'session',
-    'jquery',
-    'fourseven:scss@4.5.4',
-    'fortawesome:fontawesome@4.4.0'
-  ], 'client');
+    api.use([
+        'ecmascript',
+        'templating',
+        'session',
+        'jquery',
+        'fourseven:scss@4.5.4'
+    ], 'client');
 
-  api.addFiles([
-    'templates/bert-alert.html',
-    'templates/bert-alert.js',
-    'templates/body.html',
-    'stylesheets/colors.scss',
-    'stylesheets/flexbox.scss',
-    'stylesheets/bert.scss',
-    'bert.js',
-  ], 'client');
+    api.addFiles([
+        'templates/bert-alert.html',
+        'templates/bert-alert.js',
+        'templates/body.html',
+        'stylesheets/colors.scss',
+        'stylesheets/flexbox.scss',
+        'stylesheets/bert.scss',
+        'bert.js',
+    ], 'client');
 
-  api.export( 'Bert' );
+    api.export('Bert');
 });
 
-Package.onTest(function (api) {
-  api.use( [ 'tinytest', 'session', 'jquery' ] );
-  api.use( 'themeteorchef:bert' );
-  api.addFiles( 'tests/client.js', 'client' );
+Package.onTest(function(api) {
+    api.use(['tinytest', 'session', 'jquery']);
+    api.use('baobabcoder:bert');
+    api.addFiles('tests/client.js', 'client');
 });
